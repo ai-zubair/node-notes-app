8ea1702f352deee8a33c006859efc912dc085942
Zubair Bashir <zubairbashir@Zubairs-MacBook-Air.local>
2019-04-02 18:08:44 +0530

    expolored the lodash library

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 console.log('Starting the app now!')
-const os=require('os');
-const userData=os.userInfo()
-const noteUtils=require('./noteUtils');
-noteUtils.addNote(userData.username);
+const os = require('os');
+const _ = require('lodash');
+// const noteUtils=require('./noteUtils');
+// const userData=os.userInfo()
+// noteUtils.addNote(userData.username);
+console.log(_.isString('abc'));
+console.log(_.isString(123));
+console.log(_.uniq([1,2,1,3,4,5,1]))
