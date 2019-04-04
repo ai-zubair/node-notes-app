6915ad7a9eef8f17134dc30b1e47574c20fa1aa2
Zubair Bashir <zubair.bashirzubair@gmail.com>
2019-04-04 19:06:35 +0530

    add command definitions using yargs

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const yargs = require('yargs');
 const noteUtils = require('./utils/noteUtils');
 
-const argv = yargs.argv ;
+const titleOptions = {
+    describe:'Title of the note',
+    demand:true,
+    alias:'t'
+}
+
+const bodyOptions = {
+    describe:'Content of the note.',
+        demand:true,
+        alias:'b'
+}
+
+const addOptions = {
+    title:titleOptions,
+    body:bodyOptions
+}
+
+const commonOption ={
+    title:titleOptions
+}
+
+const argv = yargs.command('<add>','Adds a note.',addOptions)
+    .command('<list>','Lists all the saved notes.')
+    .command('<read>','Reads a saved note.',commonOption)
+    .command('<delete>','Deletes a saved note.',commonOption)
+    .help()
+    .argv ;
 
 const inputCommand = argv._[0];
 switch(inputCommand){
@@ -18,6 +44,6 @@ switch(inputCommand){
         noteUtils.deleteNote(argv.title);
         break;
     default :
-        console.log('Command not recognized');
+        console.log('App Command not recognized!');
         break;
 }
