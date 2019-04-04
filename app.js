dfaf860f8a6c42729f26313681d443d862cf0cbd
Zubair Bashir <zubair.bashirzubair@gmail.com>
2019-04-05 01:22:47 +0530

    include functionality for task status

--- app.js
+++ app.js
@@ -13,35 +13,51 @@ const taskInfoOptions = {
     alias:'i'
 }
 
+const taskStatusOptions = {
+    describe:'Current status of the task',
+    demand:true,
+    alias:'s'
+}
+
 const taskAddOptions = {
     title:taskTitleOptions,
-    info:taskInfoOptions
+    info:taskInfoOptions,
+    currStatus:taskStatusOptions
 }
 
 const commonTaskOption ={
-    title:taskTitleOptions
+    title:taskTitleOptions,
+}
+
+const setStatusOptions = {
+    title : taskTitleOptions,
+    status : taskStatusOptions
 }
 
 const argv = yargs.command('<add>','Adds a task.',taskAddOptions)
     .command('<list>','Lists all the saved tasks.')
     .command('<status>','Status of a saved task.',commonTaskOption)
     .command('<delete>','Deletes a saved task.',commonTaskOption)
+    .command('<setStatus>','Sets the status entry for a task',setStatusOptions)
     .help()
     .argv ;
 
 const userInput = argv._[0];
 switch(userInput){
     case 'add' :
-        taskUtils.addTask(argv.title,argv.info)
+        taskUtils.addTask(argv.title,argv.info,argv.currStatus)
         break;
     case 'list' :
         taskUtils.getAllTasks();
         break;
     case 'status':
-        taskUtils.getTask(argv.title);
+        taskUtils.getStatus(argv.title);
         break;
     case 'delete' :
         taskUtils.deleteTask(argv.title);
+        break;
+    case 'setStatus' :
+        taskUtils.setStatus(argv.title,argv.status);
         break;
     default :
         console.log('App Command not recognized!');
--- utils/taskUtils.js
+++ utils/taskUtils.js
@@ -28,19 +28,13 @@ const taskExists = (taskTitle) => {
 const showTask = ( task ) => {
     console.log(`\nTitle: ${task.title}\nInfo: ${task.info}\nCreated: ${task.created}\n`)
 } 
-const addTask = (taskTitle,taskInfo) => {
-    const newTask = {
-        title:taskTitle,
-        info:taskInfo,
-        created: Date()
-    } 
-    if(taskExists(taskTitle) < 0){
-        savedTasks.push(newTask);
-        saveTasks(savedTasks);
-        console.log('Task added successfully');
-        showTask(newTask);
-    } else{
-        console.log(`A task with the title ${newTask.title} already exists. Please Try With A Different Title.`)
+
+const newStatusEntry = ( taskEntry ) => {
+    if( taskEntry ){
+        return {
+            value : taskEntry,
+            time : Date()
+        }
     }
 }
 
@@ -52,10 +46,15 @@ const getAllTasks = () => {
     savedTasks.forEach( task => showTask(task));
 }
 
-const getTask = (taskTitle) => {
+const showStatus = ( task ) => {
+    for( let statusEntry of task.status ){
+        console.log(`\nStatus: ${statusEntry.value}\nTime:${statusEntry.time}`);
+    }
+} 
+const getStatus = (taskTitle) => {
     const taskIndex = taskExists(taskTitle)
     if( taskIndex > -1 ){
-        showTask(savedTasks[taskIndex]);
+        showStatus(savedTasks[taskIndex]);
     } else {
         console.log(`Ooops! Task ${taskTitle} was not found!`)
     }
@@ -73,9 +72,87 @@ const deleteTask = (taskTitle) => {
     }
 }
 
+const isValidStringStatusEntry = ( task , stringEntry) => {
+    const statusPhraseValidation = ['To-do','Just begun','In progress','Halfway','Nearly Done','Done'];
+    const stringEntryIndex = statusPhraseValidation.indexOf(stringEntry);
+    if ( stringEntryIndex < 0 ){
+        return false;
+    }
+    for( let statusEntry of task.status ){
+        if( typeof statusEntry.value === 'string' || statusPhraseValidation.indexOf(statusEntry.value) >= stringEntryIndex ){
+            return false;
+        }
+    }
+    return true;
+}
+
+const isValidNumericStatusEntry = ( task , currNumericEntry ) => {
+    for( let statusEntry of task.status ) {
+        if( typeof statusEntry.value === 'string'|| statusEntry.value >= currNumericEntry || ( currNumericEntry < 0 && currNumericEntry > 100)){
+            return false;
+        }
+    }
+    return true;
+}
+
+const validateTaskStatus = (task,taskStatusEntry) => {
+    let validationStatus;
+    const isNumericEntry = Number(taskStatusEntry);
+    if ( isNumericEntry ){
+        validationStatus = isValidNumericStatusEntry(task,isNumericEntry);
+    } else {
+        validationStatus = isValidStringStatusEntry(task,taskStatusEntry)
+    }
+    return validationStatus;
+}
+
+const appendStatusEntry = ( task , taskStatusEntry) => {
+    const isTaskStatusValid = validateTaskStatus(task,taskStatusEntry);
+    if( isTaskStatusValid ) {
+        const statusEntry = newStatusEntry(taskStatusEntry);
+        task.status.push(statusEntry);
+    } else {
+        console.log('Invalid Status Entry!')
+    }
+}
+
+const setStatus = ( taskTitle , taskStatus ) => {
+    const taskIndex = taskExists(taskTitle);
+    if( taskIndex > -1 ){
+        appendStatusEntry(savedTasks[taskIndex],taskStatus);
+        saveTasks(savedTasks);
+    } else {
+        console.log(`Ooops! Task ${taskTitle} does not exist!`)
+    }
+}
+
+const addTask = (taskTitle,taskInfo,initialStatus) => {
+    const newTask = {
+        title:taskTitle,
+        info:taskInfo,
+        created: Date(),
+        status:[]
+    }
+    const isInitialStatusValid = validateTaskStatus(newTask,initialStatus);
+    if( !isInitialStatusValid ) {
+        console.log('Invalid initial status of the task');
+        return;
+    }
+    if(taskExists(taskTitle) < 0){
+        savedTasks.push(newTask);
+        appendStatusEntry(newTask,initialStatus); 
+        saveTasks(savedTasks);
+        console.log('Task added successfully');
+        showTask(newTask);
+    } else{
+        console.log(`A task with the title ${newTask.title} already exists. Please Try With A Different Title.`)
+    }
+}
+
 module.exports = {
     addTask,
     getAllTasks,
-    getTask,
-    deleteTask
+    getStatus,
+    deleteTask,
+    setStatus
 }
